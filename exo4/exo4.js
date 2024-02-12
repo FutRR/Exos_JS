a11c77549f9d5ce8d4a2135f45beade819cc9157
FutRR <futterermaxime@gmail.com>
2024-02-12 16:57:00 +0100

    added a delay on the text transition / started an associative array for background switch

--- exo4/exo4.js
+++ exo4/exo4.js
@@ -1,7 +1,6 @@
 const box = document.querySelector(".box");
 const boxBg = window.getComputedStyle(box);
 const cards = document.querySelectorAll(".card");
-const icons = document.querySelectorAll(".card > i");
 
 const fb = document.querySelector("#fb");
 const tw = document.querySelector("#tw");
@@ -11,39 +10,45 @@ const fbIcon = document.querySelector(".fa-facebook");
 const twIcon = document.querySelector(".fa-twitter");
 const igIcon = document.querySelector(".fa-instagram");
 
-var background = {
-  "rgb(74, 74, 184)": "rgb(54, 54, 160)",
-  "rgb(79, 154, 224)": "rgb(42, 136, 224)",
-  "rgb(189, 59, 87)": "rgb(194, 36, 70)",
+var facebook = {
+  card: "rgb(66, 103, 178)",
+  background: "rgb(29, 161, 242)",
 };
 
+var twitter = {
+  card: "rgb(29, 161, 242)",
+  background: "rgb(72, 181, 248)",
+};
+
+var instagram = {
+  card: "rgb(225, 48, 108)",
+  background: "rgb(235, 70, 125)",
+};
+
+console.log(facebook);
+console.log(twitter);
+console.log(instagram);
+
 cards.forEach((card) => {
+  // We get each card individually
   card.addEventListener("click", function () {
-    card.classList.toggle("change");
+    card.classList.toggle("change"); //Toggle a class when clicked
     const cardBg = window.getComputedStyle(card);
-    var background = {
-      "rgb(74, 74, 184)": "rgb(54, 54, 160)",
-      "rgb(79, 154, 224)": "rgb(42, 136, 224)",
-      "rgb(189, 59, 87)": "rgb(194, 36, 70)",
-    };
     if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
       box.style.setProperty(
         "background-color",
         cardBg.getPropertyValue("background-color"),
         "important"
       );
-      for (var oldBg in newBg) {
-        cardBg.setProperty("background-color", +newBg, "important");
-        console.log(newBg);
-      }
-
-      cardBg.style.setProperty("background-color");
+      box.classList.add("boxBrightness");
+      cards.classList.toggle("cardBrightness");
     } else {
       box.style.setProperty(
         "background-color",
         "rgb(150, 150, 150)",
         "important"
       );
+      box.classList.remove("boxBrightness");
     }
   });
 });
@@ -54,8 +59,10 @@ fb.addEventListener("click", function () {
   const fbTextCSS = window.getComputedStyle(fbText);
   if (fbTextCSS.getPropertyValue("opacity") === "0") {
     fbText.style.setProperty("transition", "500ms ease", "important");
+    fbText.style.setProperty("transition-delay", "800ms", "important");
     fbText.style.setProperty("opacity", "1", "important");
   } else {
+    fbText.style.setProperty("transition", "500ms ease", "important");
     fbText.style.setProperty("opacity", "0", "important");
   }
 });
@@ -66,8 +73,10 @@ tw.addEventListener("click", function () {
   const twTextCSS = window.getComputedStyle(twText);
   if (twTextCSS.getPropertyValue("opacity") === "0") {
     twText.style.setProperty("transition", "500ms ease", "important");
+    twText.style.setProperty("transition-delay", "800ms", "important");
     twText.style.setProperty("opacity", "1", "important");
   } else {
+    twText.style.setProperty("transition", "500ms ease", "important");
     twText.style.setProperty("opacity", "0", "important");
   }
 });
@@ -78,8 +87,10 @@ ig.addEventListener("click", function () {
   const igTextCSS = window.getComputedStyle(igText);
   if (igTextCSS.getPropertyValue("opacity") === "0") {
     igText.style.setProperty("transition", "500ms ease", "important");
+    igText.style.setProperty("transition-delay", "800ms", "important");
     igText.style.setProperty("opacity", "1", "important");
   } else {
+    igText.style.setProperty("transition", "500ms ease", "important");
     igText.style.setProperty("opacity", "0", "important");
   }
 });
