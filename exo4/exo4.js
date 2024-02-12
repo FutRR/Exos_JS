52dc832cf8f827ababa6c7a404de29fd14c62f65
FutRR <futterermaxime@gmail.com>
2024-02-12 09:57:08 +0100

    added icon rotate and translate on click

--- exo4/exo4.js
+++ exo4/exo4.js
@@ -3,27 +3,69 @@ const boxBg = window.getComputedStyle(box);
 const cards = document.querySelectorAll(".card");
 const icons = document.querySelectorAll(".card > i");
 
-cards.forEach((card) => {
-  card.addEventListener("click", function () {
-    card.classList.toggle("change");
-    const cardBg = window.getComputedStyle(card);
-    if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
-      box.style.setProperty(
-        "background-color",
-        cardBg.getPropertyValue("background-color"),
-        "important"
-      );
-    } else {
-      box.style.setProperty(
-        "background-color",
-        "rgb(150, 150, 150)",
-        "important"
-      );
-    }
-    console.log(icons);
+const fb = document.querySelector("#fb");
+const tw = document.querySelector("#tw");
+const ig = document.querySelector("#ig");
 
-    icons.forEach((icon) => {
-      icon.classList.toggle("rotate");
-    });
-  });
+const fbIcon = document.querySelector(".fa-facebook");
+const twIcon = document.querySelector(".fa-twitter");
+const igIcon = document.querySelector(".fa-instagram");
+
+fb.addEventListener("click", function () {
+  fbIcon.classList.toggle("rotate");
+  fb.classList.toggle("change");
+  const fbBg = window.getComputedStyle(fb);
+  if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
+    box.style.setProperty(
+      "background-color",
+      fbBg.getPropertyValue("background-color"),
+      "important"
+    );
+  } else {
+    box.style.setProperty(
+      "background-color",
+      "rgb(150, 150, 150)",
+      "important"
+    );
+  }
+  fbText = document.querySelector("p");
+  fbText.style.display = "inline-block";
+});
+
+tw.addEventListener("click", function () {
+  twIcon.classList.toggle("rotate");
+  tw.classList.toggle("change");
+  const twBg = window.getComputedStyle(tw);
+  if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
+    box.style.setProperty(
+      "background-color",
+      twBg.getPropertyValue("background-color"),
+      "important"
+    );
+  } else {
+    box.style.setProperty(
+      "background-color",
+      "rgb(150, 150, 150)",
+      "important"
+    );
+  }
+});
+
+ig.addEventListener("click", function () {
+  igIcon.classList.toggle("rotate");
+  ig.classList.toggle("change");
+  const igBg = window.getComputedStyle(ig);
+  if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
+    box.style.setProperty(
+      "background-color",
+      igBg.getPropertyValue("background-color"),
+      "important"
+    );
+  } else {
+    box.style.setProperty(
+      "background-color",
+      "rgb(150, 150, 150)",
+      "important"
+    );
+  }
 });
