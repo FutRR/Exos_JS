958a60b87b75a2ac29cb82f72d2f6e2230e08763
FutRR <futterermaxime@gmail.com>
2024-02-13 11:45:53 +0100

    displayed quotes and authors in containers

--- exo5/exo5.js
+++ exo5/exo5.js
@@ -1,7 +1,24 @@
-quotesWrap = document.querySelector("#quotes");
-import { quotes } from "./quotes";
+const quotesWrap = document.querySelector("#quotes-wrap");
 
-for (key in arr) {
-  var citation = arr[key];
-  console.log(key + " : " + citation + "<br>");
-}
+import { quotes } from "../exo5/quotes.js";
+
+// Displaying quotes and authors
+Object.keys(quotes).forEach(function (writer) {
+  const citationWrap = document.createElement("div");
+  quotesWrap.appendChild(citationWrap);
+  const citation = document.createElement("i");
+  const auteur = document.createElement("p");
+  citationWrap.appendChild(citation);
+  citationWrap.appendChild(auteur);
+
+  citationWrap.classList.add("quote");
+  citation.classList.add("citation");
+  auteur.classList.add("auteur");
+
+  quotes[writer].forEach(function (quoteObj) {
+    const authorName = quoteObj.name;
+    const quote = quoteObj.quote;
+    citation.innerHTML = '"' + quote + '"';
+    auteur.innerHTML = authorName;
+  });
+});
--- exo5/quotes.js
+++ exo5/quotes.js
@@ -1,10 +1,28 @@
 export var quotes = {
-  "Serge Karamazov": "Barrez vous **** de mimes.",
-  "Georges Abitbol":
-    "Le train de tes injures roule sur le rail de mon indifférence.",
-  "Hubert Bonisseur de la Bath": "Ça ne prenait pas beaucoup plus de temps.",
-  "Moulinier ":
-    "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
-  "Orson Welles":
-    "C'est du vol et du plagiat. j'aime pas trop les voleurs et...",
+  karamazov: [{ name: "Serge Karamazov", quote: "Barrez vous **** de mimes." }],
+  abitbol: [
+    {
+      name: "Georges Abitbol",
+      quote: "Le train de tes injures roule sur le rail de mon indifférence.",
+    },
+  ],
+  bath: [
+    {
+      name: "Hubert Bonisseur de la Bath",
+      quote: "Ça ne prenait pas beaucoup plus de temps.",
+    },
+  ],
+  moulinier: [
+    {
+      name: "Moulinier ",
+      quote:
+        "L'habit ne fait pas le moine, mais il fait l'agent... Même non titularisé.",
+    },
+  ],
+  welles: [
+    {
+      name: "Orson Welles",
+      quote: "C'est du vol et du plagiat. j'aime pas trop les voleurs et...",
+    },
+  ],
 };
