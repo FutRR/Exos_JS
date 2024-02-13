af4182c9b3ae21f3de699a091e87b1ead7e846d6
FutRR <futterermaxime@gmail.com>
2024-02-13 12:01:46 +0100

    favorite icon toggle added

--- exo5/exo5.js
+++ exo5/exo5.js
@@ -7,12 +7,16 @@ Object.keys(quotes).forEach(function (writer) {
   const citationWrap = document.createElement("div");
   quotesWrap.appendChild(citationWrap);
   const citation = document.createElement("i");
+  const fav = document.createElement("i");
   const auteur = document.createElement("p");
   citationWrap.appendChild(citation);
+  citationWrap.appendChild(fav);
   citationWrap.appendChild(auteur);
 
   citationWrap.classList.add("quote");
   citation.classList.add("citation");
+  fav.classList.add("fa-regular");
+  fav.classList.add("fa-heart");
   auteur.classList.add("auteur");
 
   quotes[writer].forEach(function (quoteObj) {
@@ -20,5 +24,12 @@ Object.keys(quotes).forEach(function (writer) {
     const quote = quoteObj.quote;
     citation.innerHTML = '"' + quote + '"';
     auteur.innerHTML = authorName;
+  });
+
+  // Favorites system
+
+  fav.addEventListener("click", function () {
+    fav.classList.toggle("fa-solid");
+    fav.classList.add("fa-heart");
   });
 });
