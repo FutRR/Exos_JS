2dda9a7bc2014ec921c67b04e5dde9cd04045c1d
FutRR <futterermaxime@gmail.com>
2024-02-13 08:51:22 +0100

    comments and removal of unnecessary lines

--- exo4/exo4.js
+++ exo4/exo4.js
@@ -1,3 +1,4 @@
+// Associating html containers to constants
 const box = document.querySelector(".box");
 const boxBg = window.getComputedStyle(box);
 const cards = document.querySelectorAll(".card");
@@ -13,7 +14,8 @@ const twIcon = document.querySelector(".fa-twitter");
 const igIcon = document.querySelector(".fa-instagram");
 
 cards.forEach((card) => {
-  // We get each card individually
+  // Each card getting treated individually
+
   card.addEventListener("click", function () {
     card.classList.toggle("change"); // We add and remove a class to said card when it gets clicked
   });
@@ -39,9 +41,11 @@ console.log(facebook.background);
 console.log(twitter);
 console.log(instagram);
 
+// FACEBOOK
 fb.addEventListener("click", function () {
   fbIcon.classList.toggle("rotate"); // Icon gets a new class with rotating properties when card get clicked
 
+  // Changing background color back and forth
   if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
     box.style.setProperty("background-color", facebook.background, "important");
   } else {
@@ -50,12 +54,12 @@ fb.addEventListener("click", function () {
       "rgb(150, 150, 150)",
       "important"
     );
-    box.classList.remove("boxBrightness");
   }
 
   const fbText = document.querySelector("#fbTxt");
   const fbTextCSS = window.getComputedStyle(fbText);
 
+  // Changing text opacity on delay
   if (fbTextCSS.getPropertyValue("opacity") === "0") {
     fbText.style.setProperty("transition", "500ms ease", "important");
     fbText.style.setProperty("transition-delay", "800ms", "important");
@@ -66,9 +70,11 @@ fb.addEventListener("click", function () {
   }
 });
 
+// TWITTER
 tw.addEventListener("click", function () {
   twIcon.classList.toggle("rotate");
 
+  // Changing background color back and forth
   if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
     box.style.setProperty("background-color", twitter.background, "important");
   } else {
@@ -77,12 +83,12 @@ tw.addEventListener("click", function () {
       "rgb(150, 150, 150)",
       "important"
     );
-    box.classList.remove("boxBrightness");
   }
 
   const twText = document.querySelector("#twTxt");
   const twTextCSS = window.getComputedStyle(twText);
 
+  // Changing text opacity on delay
   if (twTextCSS.getPropertyValue("opacity") === "0") {
     twText.style.setProperty("transition", "500ms ease", "important");
     twText.style.setProperty("transition-delay", "800ms", "important");
@@ -93,9 +99,11 @@ tw.addEventListener("click", function () {
   }
 });
 
+// INSTAGRAM
 ig.addEventListener("click", function () {
   igIcon.classList.toggle("rotate");
 
+  // Changing background color back and forth
   if (boxBg.getPropertyValue("background-color") === "rgb(150, 150, 150)") {
     box.style.setProperty(
       "background-color",
@@ -108,12 +116,12 @@ ig.addEventListener("click", function () {
       "rgb(150, 150, 150)",
       "important"
     );
-    box.classList.remove("boxBrightness");
   }
 
   const igText = document.querySelector("#igTxt");
   const igTextCSS = window.getComputedStyle(igText);
 
+  // Changing text opacity on delay
   if (igTextCSS.getPropertyValue("opacity") === "0") {
     igText.style.setProperty("transition", "500ms ease", "important");
     igText.style.setProperty("transition-delay", "800ms", "important");
